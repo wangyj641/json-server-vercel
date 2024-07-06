547ae1b1f3ce9245a778768011585856c78a7bc3
Yongjun <wangyj641@gmail.com>
2024-07-06 22:38:34 +0800

    Support write permission

--- api/server.js
+++ api/server.js
@@ -4,15 +4,12 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 const middlewares = jsonServer.defaults()
 
